cf001456bec66f3935a5d5bcebc089d49feccd44
Samarth320 <samarthbagul49@gmail.com>
2024-09-27 22:18:40 +0530

    done

--- Number-System/Binary-to-decimal.cpp
+++ Number-System/Binary-to-decimal.cpp
@@ -1,33 +1,30 @@
-// C++ program to convert binary to decimal
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-// Function to convert binary to decimal
-int binaryToDecimal(int n)
-{
-	int num = n;
-	int dec_value = 0;
+    int binary_to_decimal(string str) {
+        long long ans = 0;
+        
+        long long pow_of_two = 1;
+        int n = str.length();
+        
+        for(int i=n-1; i>=0; i--)
+        {
+            if(str[i] == '1')
+            ans += pow_of_two;
+            
+            pow_of_two = pow_of_two * 2;
+        }
+        
+        return ans;
+    }
 
-	// Initializing base value to 1, i.e 2^0
-	int base = 1;
-
-	int temp = num;
-	while (temp) {
-		int last_digit = temp % 10;
-		temp = temp / 10;
-
-		dec_value += last_digit * base;
-
-		base = base * 2;
-	}
-
-	return dec_value;
-}
-
-// Driver program to test above function
 int main()
 {
-	int num = 10101001;
 
-	cout << binaryToDecimal(num) << endl;
+    string str = "10001000";
+    long long ans = binary_to_decimal(str);
+
+    cout<<"answer is "<<ans;
+
+    return 0;
 }
--- factorila.cpp
+++ factorila.cpp
@@ -1,7 +1,8 @@
-unsigned long long factorial(int n) {
-    unsigned long long result = 1;
-    for (int i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
-}
+// unsigned long long factorial(int n) {
+//     unsigned long long result = 1;
+//     for (int i = 1; i <= n; ++i) {
+//         result *= i;
+//     }
+//     return result;
+// }
+
